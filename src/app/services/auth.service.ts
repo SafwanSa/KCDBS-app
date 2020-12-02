2e4a46485e8d7d22e77e97028dc2b256d0f93e9e
Safwan <safwan9f@gmail.com>
2020-12-02 13:23:06 +0300

    User behaivoral subject to track user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { PathRequester } from '../shared/pathRequester';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  user$: Observable<User> = of(JSON.parse(localStorage.getItem('currentUser')) as User | null);
+  private currentUserSubject: BehaviorSubject<User>;
+  public user$: Observable<User>;
 
-  constructor(
-    private http: HttpClient,
-    private path: PathRequester
-  ) {
+  constructor(private http: HttpClient, private path: PathRequester) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.user$ = this.currentUserSubject.asObservable();
+  }
 
+  get user(): User {
+    return this.currentUserSubject.value;
   }
 
   get isLoggedIn(): boolean {
@@ -27,11 +30,10 @@ export class AuthService {
     let body = new HttpParams();
     body = body.set('email', email);
     body = body.set('password', password);
-    this.http.post<User>(this.path.getUser, body).subscribe(res => {
-      if (res) {
-        this.user$ = of(res);
-        localStorage.setItem('currentUser', JSON.stringify(res));
-        // localStorage.removeItem('currentUser');
+    this.http.post<User>(this.path.getUser, body).subscribe(user => {
+      if (user) {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
       }
     }, err => {
       console.log('Error while getting the user. ' + err);
@@ -40,7 +42,7 @@ export class AuthService {
 
   logout(): void {
     localStorage.removeItem('currentUser');
-    this.user$ = of(null);
+    this.currentUserSubject.next(null);
   }
 
 }
